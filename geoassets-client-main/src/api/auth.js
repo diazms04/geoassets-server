579948c0a0b280f8def49552b3d2eba21773531f
Martín Díaz <martin@Martins-MacBook-Pro.local>
2025-11-06 21:46:57 -0500

    fix: delete debug

--- geoassets-client-main/src/api/auth.js
+++ geoassets-client-main/src/api/auth.js
@@ -2,6 +2,7 @@ import { API_VERSION, HOST } from "../utils";
 
 const API_KEY = import.meta.env.VITE_API_SECRET_KEY; 
 
+
 export class Auth {
 
   async postSignUp(formData) {
@@ -16,18 +17,13 @@ export class Auth {
         body: JSON.stringify(formData)
       };
 
-      // --- LOG PARA DEPURACIÓN ---
-      console.log("POST SIGNUP - URL:", url);
-      console.log("POST SIGNUP - Headers:", params.headers);
-      console.log("POST SIGNUP - Body:", params.body);
-
       const response = await fetch(url, params);
       const result = await response.json();
       return result;
 
     } catch (error) {
-      console.log("🚀 ~ postSignUp ~ error:", error);
-      return { msg: `Error en postSignUp: ${error}`, status: false };
+      console.log();
+      return { msg: `🚀 ~ User ~ postSignUp ~ error:", ${error}`, status: false };
     }
   }
 
@@ -38,28 +34,24 @@ export class Auth {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "x-api-key": API_KEY,
+          "x-api-key": API_KEY, 
+
         },
         body: JSON.stringify(formData)
       };
-
-      // --- LOG PARA DEPURACIÓN ---
-      console.log("POST SIGNIN - URL:", url);
-      console.log("POST SIGNIN - Headers:", params.headers);
-      console.log("POST SIGNIN - Body:", params.body);
 
       const response = await fetch(url, params);
       const result = await response.json();
       return result;
 
     } catch (error) {
-      console.log("🚀 ~ postSignIn ~ error:", error);
-      return { msg: `Error en postSignIn: ${error}`, status: false };
+      console.log();
+      return { msg: `🚀 ~ User ~ postSignIn ~ error:, ${error}`, status: false };
     }
   }
 
   async refreshAccessToken(refreshToken) {
-    try {
+
       const url = `${HOST}/${API_VERSION}/auth/refresh_access_token`;
       const params = {
         method: "POST",
@@ -70,21 +62,12 @@ export class Auth {
         body: JSON.stringify({ token: refreshToken }),
       };
 
-      // --- LOG PARA DEPURACIÓN ---
-      console.log("REFRESH TOKEN - URL:", url);
-      console.log("REFRESH TOKEN - Headers:", params.headers);
-      console.log("REFRESH TOKEN - Body:", params.body);
-
       const response = await fetch(url, params);
       const result = await response.json();
 
       if (response.status !== 200) throw result;
       return result;
 
-    } catch (error) {
-      console.log("🚀 ~ refreshAccessToken ~ error:", error);
-      return { msg: `Error en refreshAccessToken: ${error}`, status: false };
-    }
   }
 
   setAccessToken(token) {
